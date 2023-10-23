14e1ac2d84642ceff4ffdd81927b4165c8507dcc
Oliver <zhuimengdisheng@outlook.com>
2023-10-23 15:58:02 +0800

    first commit

---
+++ cpp/1.two-sum.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> my_hash;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            int tmp = target - nums[i];
+            if (my_hash.count(tmp)) {
+                return {my_hash[tmp], i};
+            } else {
+                my_hash[nums[i]] = i;
+            }
+        }
+        return {-1, -1};
+    }
+};
+// @lc code=end
---
+++ python3/1.two-sum.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i in range(len(nums)):
+            if (target - nums[i]) in dic:
+                return [dic[target - nums[i]], i]
+            else:
+                dic[nums[i]] = i
+        return [-1, -1]
+
+# @lc code=end
